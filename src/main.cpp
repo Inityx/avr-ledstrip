a33f59cd7638955f255aed9718d4d6ebf577d738
Andrew Tolvstad <>
2018-12-22 01:29:53 -0800

    Residual changes

--- src/main.cpp
+++ src/main.cpp
@@ -11,23 +11,29 @@
 #include <emulated/multipwm.hpp>
 #include <peripheral/rotaryencoder.hpp>
 
-using namespace AvrSupport;
-using namespace PortLib;
-using Utility::Array;
-using Peripheral::RotaryEncoder;
+using namespace avrsupport;
+using namespace portlib;
+using namespace utility;
+using peripheral::RotaryEncoder;
 using Timer0 = Timer<uint8_t>;
 
-DigitalPort
+static DigitalPort
     port_a { PINA, PORTA, DDRA},
     port_b { PINB, PORTB, DDRB};
 
-Emulated::MultiPWM<3, 15> pwm{ // 255 is evenly divisible by 15
-    {0, 1, 2}, // Pins
+static DigitalPin
+    knob_pin_a {port_b, 0, IoDirection::in, LogicLevel::high},
+    knob_pin_b {port_b, 1, IoDirection::in, LogicLevel::high},
+    knob_button{port_a, 6, IoDirection::in, LogicLevel::high},
+     aux_button{port_a, 0, IoDirection::in, LogicLevel::high};
+
+static emulated::MultiPwm<3, 15> pwm{ // 255 is evenly divisible by 15
+    {1, 3, 5}, // Pins
     {0, 0, 0}, // Levels
     port_a
 };
 
-Timer0 timer_0{
+static Timer0 timer_0{
     TCNT0,
     OCR0A, OCR0B,
     TCCR0A, TCCR0B,
@@ -37,8 +43,11 @@ Timer0 timer_0{
 
 ISR(TIM0_OVF_vect) { pwm.step(); }
 
-void handle_buttons(bool knob_button, bool aux_button) {
-    if (knob_button) {
+void handle_buttons(
+    LogicLevel const knob_button,
+    LogicLevel const aux_button
+) {
+    if (knob_button == LogicLevel::low) {
         pwm.pause();
         
         pwm.select_next();
@@ -48,7 +57,7 @@ void handle_buttons(bool knob_button, bool aux_button) {
         pwm.resume();
     }
 
-    if (aux_button) {
+    if (aux_button == LogicLevel::low) {
         // TODO: Select next preset
         pwm.pause();
         pwm.isolate_selected();
@@ -68,22 +77,6 @@ void handle_knob_rotate(RotaryEncoder & knob) {
 int main() {
     RotaryEncoder knob;
 
-    DigitalPin
-        knob_pin_a {port_b, 2},
-        knob_pin_b {port_b, 1},
-        knob_button{port_a, 4},
-         aux_button{port_a, 3};
-
-    Array<DigitalPin *, 4> inputs {
-        &knob_pin_a,
-        &knob_pin_b,
-        &knob_button,
-        &aux_button
-    };
-
-    for (auto input : inputs)
-        (*input).set_in().set_high();
-    
     pwm.set_pins_out();
 
     timer_0
@@ -94,8 +87,11 @@ int main() {
     sei();
 
     while(true) {
-        handle_buttons(!knob_button.get(), !aux_button.get());
-        knob.sample(!knob_pin_a.get(), !knob_pin_b.get());
+        handle_buttons(knob_button.get(), aux_button.get());
+        knob.sample(
+            !static_cast<bool>(knob_pin_a.get()),
+            !static_cast<bool>(knob_pin_b.get())
+        );
         handle_knob_rotate(knob);
 
         // Debounce
